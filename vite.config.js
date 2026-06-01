5e1e25e9753a9f2f1173ecc17aa5a92f1a9dd374
d <david.vakhayev@gmail.com>
2026-06-01 16:32:20 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/serviceflow-booking-system/",
   plugins: [react()],
   server: {
     port: 5173,
-    open: true
-  }
+    open: true,
+  },
 });
